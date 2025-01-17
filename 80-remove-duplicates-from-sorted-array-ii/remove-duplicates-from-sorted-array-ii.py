f97ff5dba5c2c523d9dc5c0398bfddee36bc0e2b
Muhammad Taqi Raza <122045573+TaqiRaza512@users.noreply.github.com>
2025-01-17 08:06:41 +0500

    Time: 52 ms (65.90%) | Memory: 17.9 MB (10.08%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -5,9 +5,6 @@ class Solution:
         prev_value = nums[0]
         i = 1
         while (i < len(nums) - discarded):
-
-            print(i)
-        
             if nums[i] == prev_value:
                 count += 1
             else:
@@ -21,7 +18,6 @@ class Solution:
                 for c in range(i,len(nums)-1):
                     nums[c] = nums[c+1]
                 i -= 1
-              
             i += 1
 
         return len(nums) - discarded
