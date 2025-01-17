2065d47b6fde617ba2f9b8b97dd340ff7aa3a5a1
Muhammad Taqi Raza <122045573+TaqiRaza512@users.noreply.github.com>
2025-01-17 17:41:31 +0500

    Time: 56 ms (40.63%) | Memory: 17.9 MB (22.08%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,23 +1,34 @@
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         count = 1
+#         discarded = 0
+#         prev_value = nums[0]
+#         i = 1
+#         while (i < len(nums) - discarded):
+#             if nums[i] == prev_value:
+#                 count += 1
+#             else:
+#                 count = 1
+
+#             prev_value = nums[i]
+        
+#             if count > 2:
+#                 discarded += 1
+                
+#                 for c in range(i,len(nums)-1):
+#                     nums[c] = nums[c+1]
+#                 i -= 1
+#             i += 1
+#         return len(nums) - discarded
+
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        count = 1
-        discarded = 0
-        prev_value = nums[0]
-        i = 1
-        while (i < len(nums) - discarded):
-            if nums[i] == prev_value:
-                count += 1
-            else:
-                count = 1
+        k = 2
 
-            prev_value = nums[i]
-        
-            if count > 2:
-                discarded += 1
-                
-                for c in range(i,len(nums)-1):
-                    nums[c] = nums[c+1]
-                i -= 1
-            i += 1
+        for i in range(2, len(nums)):
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1 
 
-        return len(nums) - discarded
+        return k
