c574d84707e6f19bc957e6341b26b21c50ef30ac
Muhammad Taqi Raza <122045573+TaqiRaza512@users.noreply.github.com>
2025-03-01 23:04:24 +0500

    Time: 4 ms (9.53%) | Memory: 17.9 MB (42.91%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -3,19 +3,10 @@ class Solution:
         small_pointer = 0
         large_pointer = 0
         
-        if len(s) == 0:
-            return True
-
         while large_pointer < len(t) and small_pointer < len(s):
-
             if s[small_pointer] == t[large_pointer]:
                 small_pointer += 1
-                large_pointer += 1
-            else:
-                large_pointer += 1
+            large_pointer += 1
 
-            if small_pointer == len(s):
-                return True
-            
-        return False
+        return small_pointer == len(s)
         
